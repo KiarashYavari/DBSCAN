acfe8f1e6c6ab8f3eb2777f7f5bed2077a6e9a1a
KiarashYavari <kiarash996@gmail.com>
2021-08-09 14:27:59 +0430

    create a table and put records inside it from edited_dataset file

--- base/models.py
+++ base/models.py
@@ -5,15 +5,39 @@ import pandas as pd
 # create models from each cluster
 from django.db import models
 
-dataset = pd.read_csv('../files/edited_dataset.csv')
-dataset = dataset.drop(labels='Unnamed: 0', axis='1')
-cluster_numbers = dataset['clusters'].value_counts()
+dataset = pd.read_csv('E://Prj-karshenasi/webApp_codes/files/edited_dataset.csv')
 
-# https://stackoverflow.com/questions/5036357/single-django-model-multiple-tables
-# def getModel():
-#     class Clusters(models.Model):
-#
-#         class Meta:
-#             db_table =
-#
-#     return Clusters
+
+class Cluster(models.Model):
+    skills = models.IntegerField(null=False, default=1)
+    pre_skills = models.BooleanField(null=False, default=0)
+    starting_point = models.IntegerField(null=False, default=1)
+    English_level = models.IntegerField(null=False, default=1)
+    time_interval = models.IntegerField(null=False, default=1)
+    skill_pros = models.BooleanField(null=False, default=1, max_length=1)
+    skill_profit = models.BooleanField(null=False, default=1, max_length=1)
+    skill_attraction = models.BooleanField(null=False, default=1, max_length=1)
+    i_e_parameter = models.CharField(max_length=1, null=False, default='I')
+    s_n_parameter = models.CharField(max_length=1, null=False, default='S')
+    t_f_parameter = models.CharField(max_length=1, null=False, default='T')
+    p_j_parameter = models.CharField(max_length=1, null=False, default='P')
+    clusters = models.FloatField(null=False, default=1)
+
+# records added
+# for index in range(len(dataset)):
+#     new_class = Cluster()
+#     new_class.skills = dataset.iloc[index, 1]
+#     new_class.pre_skills = dataset.iloc[index, 2]
+#     new_class.starting_point = dataset.iloc[index, 3]
+#     new_class.English_level = dataset.iloc[index, 4]
+#     new_class.time_interval = dataset.iloc[index, 5]
+#     new_class.skill_pros = dataset.iloc[index, 6]
+#     new_class.skill_profit = dataset.iloc[index, 7]
+#     new_class.skill_attraction = dataset.iloc[index, 8]
+#     new_class.i_e_parameter = dataset.iloc[index, 9]
+#     new_class.s_n_parameter = dataset.iloc[index, 10]
+#     new_class.t_f_parameter = dataset.iloc[index, 11]
+#     new_class.p_j_parameter = dataset.iloc[index, 12]
+#     new_class.clusters = dataset.iloc[index, 13]
+#     new_class.save()
+
