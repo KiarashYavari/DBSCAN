5b8b22f61dd1476b8bb56b10e3e21ecfad6c0ec6
KiarashYavari <kiarash996@gmail.com>
2021-08-09 14:31:49 +0430

    detect personality factors and cluster records with dbscan

--- DBSCAN/DBSCAN_Algorithm.py
+++ DBSCAN/DBSCAN_Algorithm.py
@@ -11,12 +11,12 @@ from sklearn.decomposition import PCA
 
 # needed libraries installed and imported
 # read data and remove user_ID
-data = pd.read_csv('../files/edited_dataset.csv')
+data = pd.read_csv('../files/edited_dataset1.csv')
 
 data = data.drop('شناسه پاسخ‌دهنده', axis=1)
 data = data.drop(index=0)  # drop headers
 data = data.drop(index=1)
-# print(data.head())
+print(data.head())
 data.to_csv('../files/edited_dataset.csv')
 # Scaling the data to bring all the attributes to a comparable level
 scaler = StandardScaler()  # create an object from StandardScaler
@@ -84,6 +84,44 @@ labels = db_default.labels_
 # saving clusters into the ../files/edited_dataset.csv
 clusters = labels
 df = pd.read_csv("../files/edited_dataset.csv")
+# calculate personality type
+# i-e s-n t-f p-j
+for pointer in range(53):
+    i_e_row = df.iloc[[pointer], [9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65]]
+    i_e_counter = i_e_row[i_e_row.values == 1].shape[0]
+
+    i_e_different = abs(15 - i_e_counter)
+    dataframe = df
+    if i_e_different < i_e_counter:
+        dataframe.iloc[[pointer], [9]] = 'I'
+    else:
+        dataframe.iloc[[pointer], [9]] = 'E'
+
+    s_n_row = df.iloc[[pointer], [10, 14, 18, 22, 26, 30, 34, 38, 42, 46, 50, 54, 58, 62, 66]]
+    s_n_counter = s_n_row[s_n_row.values == 1].shape[0]
+    s_n_different = abs(15 - s_n_counter)
+    if s_n_different < s_n_counter:
+        dataframe.iloc[[pointer], [10]] = 'S'
+    else:
+        dataframe.iloc[[pointer], [10]] = 'N'
+
+    t_f_row = df.iloc[[pointer], [11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63, 67]]
+    t_f_counter = t_f_row[t_f_row.values == 1].shape[0]
+    t_f_different = abs(15 - t_f_counter)
+    if t_f_different < t_f_counter:
+        dataframe.iloc[[pointer], [11]] = 'T'
+    else:
+        dataframe.iloc[[pointer], [11]] = 'F'
+
+    p_j_row = df.iloc[[pointer], [12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68]]
+    p_j_counter = p_j_row[i_e_row.values == 1].shape[0]
+    p_j_different = abs(15 - p_j_counter)
+    if p_j_different < p_j_counter:
+        dataframe.iloc[[pointer], [12]] = 'P'
+    else:
+        dataframe.iloc[[pointer], [12]] = 'J'
+# end of calculate
+df.drop(df.iloc[:, 13:69], axis=1, inplace=True)
 df['clusters'] = clusters
 df.to_csv("../files/edited_dataset.csv")
 # add cluster column -- two indexing extra columns
