957f88df3d8531b0d59b908b168202338a6f88ee
KiarashYavari <kiarash996@gmail.com>
2021-08-12 11:05:17 +0430

    design the base pages view, add stylesheets

--- base/forms.py
+++ base/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class PersonalityFactors(forms.Form):
-    p_factors = forms.CharField(label="فاکتور های شخصیت شما:", max_length=4, min_length=4,
+    p_factors = forms.CharField(label=False, max_length=4, min_length=4,
                                 widget=forms.TextInput(attrs={'placeholder': 'INTJ'}))
