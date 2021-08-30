33daeb784d0d8dace7fa3403c5d023e88b38a22a
kiayavari <64461860+KiarashYavari@users.noreply.github.com>
2021-08-30 11:19:09 +0430

    Update settings.py

--- webApp_codes/settings.py
+++ webApp_codes/settings.py
@@ -27,7 +27,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-x-z$7q7^bq+cw=i_j^=0z%y1cfg7df-c@h0e%#3yuh5tj!=dr$'
 SECRET_KEY = get_random_secret_key()
 # dotenv_file = os.path.join(BASE_DIR, ".env")
 # if os.path.isfile(dotenv_file):
